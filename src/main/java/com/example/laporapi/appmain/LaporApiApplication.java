ce0696a18f225c4024bcb37519cf062198653028
Muhammad Hazim <mhazim2@gmail.com>
2018-04-20 02:58:05 +0700

    Initial commit

---
+++ src/main/java/com/example/laporapi/appmain/LaporApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.laporapi.appmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaporApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaporApiApplication.class, args);
+	}
+}
