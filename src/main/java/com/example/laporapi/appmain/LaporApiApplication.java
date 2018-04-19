c51834527a4868b117bea806ce9f25be957d13b1
Muhammad Hazim <mhazim2@gmail.com>
2018-04-20 03:14:02 +0700

    initial class

--- src/main/java/com/example/laporapi/appmain/LaporApiApplication.java
+++ src/main/java/com/example/laporapi/appmain/LaporApiApplication.java
@@ -2,8 +2,10 @@ package com.example.laporapi.appmain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.laporapi")
 public class LaporApiApplication {
 
 	public static void main(String[] args) {
